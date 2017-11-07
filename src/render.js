b750b9109186dbf74827e48371a01bee846bfeee
Garry Blight <garry.blight@guardian.co.uk>
2017-11-07 17:09:46 +0000

    New data

--- src/render.js
+++ src/render.js
@@ -7,7 +7,8 @@ const Page = requireUncached('../src/components/page/render.html')
 
 export async function render() {
   const data = await rp({
-      uri: 'https://interactive.guim.co.uk/docsdata-test/1P-6YwejZEA68ZzJVsJV0_jWJL-7uYOwEzFRi_UTSdfU.json',
+      //uri: 'https://interactive.guim.co.uk/docsdata-test/1P-6YwejZEA68ZzJVsJV0_jWJL-7uYOwEzFRi_UTSdfU.json',
+      uri: 'https://interactive.guim.co.uk/docsdata-test/1PQpJPmf090eXCxRbsxPZJH6QCfElZx0MguTfg94qft0.json',
       json: true
   });
 
