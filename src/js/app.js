0d4d310cb5f5060f275ce8075c61eaea88a39714
Josh Holder <josh.holder@theguardian.com>
2017-11-13 14:51:23 +0000

    Add videos and grid changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,9 @@
 import YouTubePlayer from '../components/youtube-player/index.html'
+import VideoPlayer from '../components/video/index.html'
 
 //initialise YouTube player
 const YouTubeEls = document.querySelectorAll(".visual-element--youtube");
 let players = [];
-
-console.log("test");
 
 [].slice.apply(YouTubeEls).forEach((el) => {
     const player = new YouTubePlayer({
@@ -17,3 +16,22 @@ console.log("test");
     })
     players.push(player);
 }); 
+
+
+//initialise video player
+const VideoEls = document.querySelectorAll(".visual-element--video");
+let videoPlayers = [];
+
+[].slice.apply(VideoEls).forEach((el) => {
+    const player = new VideoPlayer({
+    	target: el,
+        data: {
+            loop: true,
+            targetEl: el,
+            mobileSrc: el.getAttribute("data-mobileSrc"),
+            desktopSrc: el.getAttribute("data-desktopSrc")
+        }
+    })
+    videoPlayers.push(player);
+}); 
+
